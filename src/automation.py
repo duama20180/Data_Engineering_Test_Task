f56a4d2840ff59d00e907888aaa4b5adb61c6b92
duama20180 <accforwork20180@gmail.com>
2025-10-02 13:18:33 +0300

    Update errors handling via automation.py, Add more info about pipeline while it's running

--- src/automation.py
+++ src/automation.py
@@ -2,15 +2,30 @@ from extract import extract_data
 from transform import transforming_stage
 from load import loading_stage
 from analytics import analysing_stage
+import sys
 
-#stage extracting
-extract_data()
+def main():
+    try:
+        print("Starting pipeline")
 
-#stage transforming
-transforming_stage()
+        print("Extracting data")
+        extract_data()
 
-#stage loading to db
-loading_stage()
+        print("Transforming data")
+        transforming_stage()
 
-#data analysis stage
-analysing_stage()
+        print("Loading data into DB")
+        loading_stage()
+
+        print("Running analytics")
+        analysing_stage()
+
+        print("Pipeline finished successfully!")
+
+    except Exception as e:
+        print(f"Pipeline failed: {e}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
